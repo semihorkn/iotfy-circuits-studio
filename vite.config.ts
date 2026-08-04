685a02e20bd68bb7eda9acc6f179441a3e2350e0
semihorkn <149263807+semihorkn@users.noreply.github.com>
2026-08-04 11:35:02 +0100

    Publish IOTfy Circuits Studio with GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: process.env.GITHUB_ACTIONS ? '/iotfy-circuits-studio/' : '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
